9f0ab726c7dfb7c39335b296bf231bea422be4d8
Rinat <swatlprus@gmail.com>
2023-08-23 09:07:26 +0500

    Improved question parsing

--- learn_bot.py
+++ learn_bot.py
@@ -1,17 +1,22 @@
 import re
+import os
 
 
-with open('quiz-questions/1vs1200.txt', 'r', encoding='KOI8-R') as file_questions:
-  texts = file_questions.read().split('\n\n\n')
+def read_questions(quiz_folder):
+    files = os.listdir(quiz_folder)
+    questions = {}
+    for file in files:
+        filepath = os.path.join(quiz_folder, file)
 
+        with open(filepath, 'r', encoding='KOI8-R') as file_questions:
+            texts = file_questions.read().split('\n\n\n')
 
-questions = {}
-for text in texts:
-    if len(text) != 0:
-        start_question_index = re.search(r'Вопрос\s\d*:', text).end()
-        end_question_index = re.search('Ответ:', text).start()
-        start_answer_index = re.search('Ответ:', text).end()
-        end_answer_index = re.search('Автор:', text).start()
-        question = text[start_question_index:end_question_index]
-        answer = text[start_answer_index:end_answer_index]
-        questions[question] = answer
+        for text in texts:
+            blocks = text.split('\n\n')
+            for block in blocks:
+                if 'Вопрос' in block:
+                    question = re.sub('Вопрос\D\d*:\n', '', block)
+                elif 'Ответ' in block:
+                    answer = re.sub('Ответ:\n', '', block)
+            questions[question] = answer
+    return questions
--- tg_bot.py
+++ tg_bot.py
@@ -1,21 +1,27 @@
 import logging
 import requests
 import time
+import functools
+import random
+import redis
 from environs import Env
 
-from telegram import Update, ForceReply
+import telegram
+from telegram import Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from learn_bot import read_questions
 
 logger = logging.getLogger('Logger')
 
 
-def start(update: Update, context: CallbackContext) -> None:
-    """Send a message when the command /start is issued."""
-    user = update.effective_user
-    update.message.reply_markdown_v2(
-        fr'Здравствуйте {user.mention_markdown_v2()}\!',
-        reply_markup=ForceReply(selective=True),
-    )
+def start_bot(tg_token, update: Update, context: CallbackContext) -> None:
+    custom_keyboard = [['Новый вопрос', 'Сдаться'],
+                       ['Мой счёт']]
+    reply_markup = telegram.ReplyKeyboardMarkup(custom_keyboard)
+    bot = telegram.Bot(token=tg_token)
+    bot.send_message(chat_id=update.message.chat_id,
+                     text="Привет! Я бот для викторин!",
+                     reply_markup=reply_markup)
 
 
 def help_command(update: Update, context: CallbackContext) -> None:
@@ -23,19 +29,22 @@ def help_command(update: Update, context: CallbackContext) -> None:
     update.message.reply_text('Help!')
 
 
-def echo(update: Update, context: CallbackContext) -> None:
-    """Echo the user message."""
-    update.message.reply_text(update.message.text)
+def send_question(questions, update: Update, context: CallbackContext) -> None:
+    message = update.effective_message
+    if message.text == 'Новый вопрос':
+        update.message.reply_text(random.choice(list(questions.keys())))
 
 
-def main(tg_token) -> None:
+def main(tg_token, questions) -> None:
     updater = Updater(tg_token)
     dispatcher = updater.dispatcher
 
+    start = functools.partial(start_bot, tg_token)
     dispatcher.add_handler(CommandHandler("start", start))
     dispatcher.add_handler(CommandHandler("help", help_command))
 
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, echo))
+    random_question = functools.partial(send_question, questions)
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, random_question))
 
     updater.start_polling()
     updater.idle()
@@ -49,9 +58,13 @@ if __name__ == '__main__':
     env = Env()
     env.read_env()
     tg_token = env('TG_TOKEN')
+    quiz_folder = env('QUIZ_FOLDER')
+    questions = read_questions(quiz_folder)
+    pool = redis.ConnectionPool(host='localhost', port=6379, db=0)
+    r = redis.Redis(connection_pool=pool)
     logger.info('Telegram бот начал работу')
     try:
-        main(tg_token)
+        main(tg_token, questions)
     except requests.exceptions.HTTPError:
         logger.error('Telegram бот упал с ошибкой HTTPError')
     except requests.exceptions.ReadTimeout:
